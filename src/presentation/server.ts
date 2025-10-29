019e5692da1aff3bfd9957ffa4aa51cea7fff78b
Retford <retfordjb@gmail.com>
2025-10-28 19:35:10 -0500

    crud added

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
   port: number;
+  routes: Router;
   public_path?: string;
 }
 
@@ -10,25 +11,40 @@ export class Server {
   private app = express();
   private readonly port: number;
   private readonly publicPath: string;
+  private readonly routes: Router;
 
   constructor(options: Options) {
-    const { port, public_path = 'public' } = options;
+    const { port, routes, public_path = 'public' } = options;
     this.port = port;
     this.publicPath = public_path;
+    this.routes = routes;
   }
 
   async start() {
     //* middlewares
+    this.app.use(express.json()); // json
+    this.app.use(express.urlencoded({ extended: true })); //LINK - x-www-form-urlencoded
 
     //* public folder
     this.app.use(express.static(this.publicPath));
 
-    this.app.use((req, res) => {
+    //* Routes
+    this.app.use(this.routes);
+
+    //* SPA
+    this.app.get('/{*splat}', (req, res) => {
       const indexPath = path.join(
         __dirname + `../../../${this.publicPath}/index.html`
       );
       res.sendFile(indexPath);
     });
+
+    // this.app.use((req, res) => {
+    //   const indexPath = path.join(
+    //     __dirname + `../../../${this.publicPath}/index.html`
+    //   );
+    //   res.sendFile(indexPath);
+    // });
 
     this.app.listen(this.port, () => {
       console.log(`Server running on port ${this.port}`);
---
+++ src/presentation/todos/controller.ts
@@ -0,0 +1,82 @@
+import { error } from 'console';
+import { Request, Response } from 'express';
+
+const todos = [
+  { id: 1, text: 'Buy milk', completedAt: new Date() },
+  { id: 2, text: 'Buy rice', completedAt: null },
+  { id: 3, text: 'Buy juice', completedAt: new Date() },
+];
+
+export class TodosController {
+  //* Inducción de dependencias
+  constructor() {}
+
+  public getTodos = (req: Request, res: Response) => {
+    return res.json(todos);
+  };
+
+  public getTodoByID = (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+
+    if (isNaN(id))
+      return res.status(400).json({ error: 'ID argument is not a number' });
+
+    const todo = todos.find((todo) => todo.id === id);
+
+    todo
+      ? res.status(200).json(todo)
+      : res.status(404).json({ error: `TODO with id ${id} not fund` });
+  };
+
+  public createTodo = (req: Request, res: Response) => {
+    const { text } = req.body;
+    if (!text)
+      return res.status(400).json({ error: 'Text property is required' });
+
+    const newTodo = {
+      id: todos.length + 1,
+      text,
+      completedAt: null,
+    };
+    todos.push(newTodo);
+
+    return res.json(todos);
+  };
+
+  public updateTodo = (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+
+    if (isNaN(id))
+      return res.status(400).json({ error: 'ID argument is not a number' });
+
+    const todo = todos.find((todo) => todo.id === id);
+
+    if (!todo)
+      return res.status(404).json({ error: `Todo with id ${id} not found` });
+
+    const { text, completedAt } = req.body;
+
+    todo.text = text || todo.text;
+    completedAt === null
+      ? (todo.completedAt = null)
+      : (todo.completedAt = new Date(completedAt || todo.completedAt));
+
+    return res.json(todo);
+  };
+
+  public deleteTodo = (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+
+    if (isNaN(id))
+      return res.status(400).json({ error: 'ID argument is not a number' });
+
+    const todo = todos.find((todo) => todo.id === id);
+
+    if (!todo)
+      return res.status(404).json({ error: `Todo with id ${id} not found` });
+
+    todos.splice(todos.indexOf(todo), 1);
+
+    return res.json(todo);
+  };
+}

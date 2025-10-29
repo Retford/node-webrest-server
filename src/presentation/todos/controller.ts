2f068013b24ea76c5ebe923651d1e3e9fbb21b31
Retford <retfordjb@gmail.com>
2025-10-29 18:43:50 -0500

    .

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
-import { prisma } from '../../data/postgres';
+// import { prisma } from '../../data/postgres';
+import { prisma } from '../../data/postgres/index';
 import { CreateTodoDto } from '../../domain/dtos/todos/create-todo.dto';
 import { UpdateTodoDto } from '../../domain/dtos/todos/update-todo.dto';
 
